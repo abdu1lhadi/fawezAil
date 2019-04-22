d94a1d94abdd84ba0ddcc6fd7924fec48d3e8071
Abdu1lhadi <abxxdo@gmail.com>
2019-04-22 22:27:35 +0300

    Updated static_root

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 #if you want to make Static folder for all project you can use this code
