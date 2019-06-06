2b38bae40ed7315c19d71d75a9ad87513fc4e831
Abdu1lhadi <abxxdo@gmail.com>
2019-06-06 04:49:22 +0300

    add image view

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns =[
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('detail/<int:post_id>/', views.post_detail, name='detail')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
